7181ece1d504b07b25261b72132fb29e6e9b49ef
legoraft <50689727+legoraft@users.noreply.github.com>
2023-12-10 23:31:31 +0100

    Moved all parsing code to `parser.rs`

--- src/main.rs
+++ src/main.rs
@@ -2,56 +2,31 @@ mod parser;
 
 use std::env::args;
 use std::fs::File;
-use std::io::{BufRead, BufReader, Write};
-use std::path::Path;
-use crate::parser::heading;
+use std::io::Write;
 
 fn main() {
     let arguments: Vec<String> = args().collect();
 
     match &arguments.len() {
-        2 => parse_file(&arguments[1]),
+        2 => compile_file(&arguments[1]),
         _ => usage(),
+    }
+}
+
+fn compile_file(filename: &str) {
+    let output_filename = String::from(format!("{}.html", &filename[..&filename.len() - 3]));
+    let tokens = parser::parse(filename);
+
+    let mut output_file = File::create(&output_filename)
+        .expect("Error, could not create output file!");
+
+    for line in &tokens {
+        output_file.write_all(line.as_bytes()).expect("Error, failed to write line!");
     }
 }
 
 fn usage() {
     full_banner();
-}
-
-fn parse_file(filename: &str) {
-    let mut tokens: Vec<String> = Vec::new();
-    let mut output_filename = String::from(&filename[..&filename.len() - 3]);
-    output_filename.push_str(".html");
-
-    let mut output_file = File::create(&output_filename)
-        .expect("Error, could not create output file!");
-
-    let input_filename = Path::new(filename);
-    let file = File::open(input_filename).expect("Error, failed to read file!");
-    let reader = BufReader::new(file);
-
-    for line in reader.lines() {
-        let line_content = line.unwrap();
-        let mut output_line: String;
-
-        if line_content.contains("# ") {
-            output_line = heading::compile(line_content);
-        } else {
-            output_line = format!("<p>{}</p>\n", &line_content);
-        }
-
-        if output_line != "<p></p>\n" {
-            tokens.push(output_line);
-        }
-    }
-
-    short_banner();
-    println!("Parsing file {:?}...", &filename);
-    for line in &tokens {
-        output_file.write_all(line.as_bytes()).expect("Error, failed to write line!");
-    }
-    println!("Finished parsing file, check {:?}!", &output_filename);
 }
 
 fn full_banner() {
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,1 +1,30 @@
-pub mod heading;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+
+mod heading;
+
+pub fn parse(filename: &str) -> Vec<String> {
+    let mut tokens: Vec<String> = Vec::new();
+
+    let input_filename = Path::new(filename);
+    let file = File::open(input_filename).expect("Error, failed to read file!");
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        let line_content = line.unwrap();
+        let output_line: String;
+
+        if line_content.contains("# ") {
+            output_line = heading::compile(line_content);
+        } else {
+            output_line = format!("<p>{}</p>\n", &line_content);
+        }
+
+        if output_line != "<p></p>\n" {
+            tokens.push(output_line);
+        }
+    }
+
+    tokens
+}
